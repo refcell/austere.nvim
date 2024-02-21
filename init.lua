caea862a3625a821e4ac57ed84d7e3b6468b1cca
refcell <abigger87@gmail.com>
2024-02-21 13:40:09 -0500

    feat: basic neotree setup

--- init.lua
+++ init.lua
@@ -18,6 +18,29 @@ lazy.setup({
       -- refer to the configuration section below
     }
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    -- keys = { { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true } },
+    opts = {
+	filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false,
+        },
+      },
+      window = { position = "right" },
+    }
+  },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "folke/neoconf.nvim",
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,6 +8,7 @@ vim.keymap.set("n", "-", vim.cmd.Ex) -- need nvim 0.8+
 vim.keymap.set("n", "<C-p>", "<cmd>Lazy profile<cr>", { silent = true, desc = "Lazy Profile" })
 
 -- Files --
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree<cr>", { silent = true, desc = "Neotree" })
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { silent = true, desc = "Save" })
 vim.keymap.set("n", "<leader>q", "<cmd>confirm q<cr>", { silent = true, desc = "Quit" })
 vim.keymap.set("n", "<leader>n", "<cmd>enew<cr>", { silent = true, desc = "New File"})
@@ -19,3 +20,6 @@ vim.keymap.set("n", "<C-q>", "<cmd>qa!<cr>", { silent = true, desc = "Force quit
 vim.keymap.set("n", "|", "<cmd>vsplit<cr>", { silent = true, desc = "Vertical Split" })
 vim.keymap.set("n", "\\", "<cmd>split<cr>", { silent = true, desc = "Horizontal Split" }) 
 
+-- Navigate tabs
+vim.keymap.set("n", "]t", function() vim.cmd.tabnext() end, { silent = true, desc = "Next Tab" })
+vim.keymap.set("n", "[t", function() vim.cmd.tabprevious() end, { silent = true, desc = "Previous Tab" })
