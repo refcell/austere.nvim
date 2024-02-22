2a563c5d5a9f9ed78e26aa13d7f151f0d4def271
refcell <abigger87@gmail.com>
2024-02-21 19:49:02 -0500

    feat: general cleanup and styling

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@
 require "mappings"
 require "plugins"
 
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = false
+
 -- Vim Spacing --
 vim.o.expandtab = true
 vim.o.smartindent = true
@@ -11,15 +15,21 @@ vim.o.shiftwidth = 2
 -- Load External Plugins --
 local lazy = require "lazy"
 lazy.setup({
+  { "tiagovla/scope.nvim" },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
   {
     "akinsho/toggleterm.nvim",
     cmd = { "ToggleTerm", "TermExec" },
     opts = {
       highlights = {
-        Normal = { link = "Normal" },
+        -- Normal = { link = "Normal" },
         NormalNC = { link = "NormalNC" },
-        NormalFloat = { link = "NormalFloat" },
-        FloatBorder = { link = "FloatBorder" },
+        -- NormalFloat = { link = "NormalFloat" },
+        -- FloatBorder = { link = "FloatBorder" },
         StatusLine = { link = "StatusLine" },
         StatusLineNC = { link = "StatusLineNC" },
         WinBar = { link = "WinBar" },
@@ -31,7 +41,8 @@ lazy.setup({
         vim.opt.signcolumn = "no"
       end,
       open_mapping = [[<C-\>]],
-      shading_factor = 2,
+      -- shade_terminals = false,
+      -- shading_factor = 100,
       direction = "float",
       float_opts = { border = "rounded" },
     },
@@ -104,11 +115,29 @@ lazy.setup({
   "folke/neodev.nvim",
 })
 
+-- Set Telescope Keybindings --
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = "Find word under cursor" })
+vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = "Find commands" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find help tags" })
+vim.keymap.set('n', '<leader>fm', builtin.man_pages, { desc = "Find man pages" })
+vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = "Find registers" })
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "Find words" })
+vim.keymap.set('n', '<leader>fW', function()
+  builtin.live_grep {
+    additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+  }
+end, { desc = "Find words in all files" })
+
 -- Set the colorscheme --
 local theme = require "theme"
 vim.cmd.colorscheme 'catppuccin'
 require("catppuccin").setup(theme)
 
+require("scope").setup({})
 require("toggleterm").setup{}
-
 require("noicecfg")()
