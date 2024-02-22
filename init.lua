a97861d053f2231fbef83675a97c69285dfc2f6d
refcell <abigger87@gmail.com>
2024-02-22 00:50:01 -0500

    feat: clean up lualine

--- init.lua
+++ init.lua
@@ -2,9 +2,16 @@
 require "mappings"
 require "plugins"
 
--- Line numbers
+-- Line numbers --
 vim.opt.number = true
 vim.opt.relativenumber = false
+vim.opt.cursorline = true
+
+-- Folds --
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 
 -- Vim Spacing --
 vim.o.expandtab = true
@@ -113,7 +120,41 @@ lazy.setup({
     dependencies = { 'nvim-tree/nvim-web-devicons' }
   },
   "folke/neodev.nvim",
+  -- {
+  --   "kevinhwang91/nvim-ufo",
+  --   event = { "User AstroFile", "InsertEnter" },
+  --   dependencies = { "kevinhwang91/promise-async" },
+  --   opts = {
+  --     preview = {
+  --       mappings = {
+  --         scrollB = "<C-b>",
+  --         scrollF = "<C-f>",
+  --         scrollU = "<C-u>",
+  --         scrollD = "<C-d>",
+  --       },
+  --     },
+  --     provider_selector = function(_, filetype, buftype)
+  --       local function handleFallbackException(bufnr, err, providerName)
+  --         if type(err) == "string" and err:match "UfoFallbackException" then
+  --           return require("ufo").getFolds(bufnr, providerName)
+  --         else
+  --           return require("promise").reject(err)
+  --         end
+  --       end
+  --
+  --       return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+  --         or function(bufnr)
+  --           return require("ufo")
+  --             .getFolds(bufnr, "lsp")
+  --             :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
+  --             :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
+  --         end
+  --     end,
+  --   },
+  -- },
 })
+
+-- require('ufo').setup({})
 
 -- Set Telescope Keybindings --
 local builtin = require('telescope.builtin')
@@ -141,3 +182,5 @@ require("catppuccin").setup(theme)
 require("scope").setup({})
 require("toggleterm").setup{}
 require("noicecfg")()
+
+require 'bubbles'
---
+++ lua/bubbles.lua
@@ -0,0 +1,71 @@
+local mocha = require("catppuccin.palettes").get_palette "mocha"
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = mocha.text, bg = mocha.surface0 },
+    b = { fg = mocha.text, bg = mocha.surface1 },
+    c = { fg = mocha.text, bg = mocha.base },
+  },
+
+  insert = { a = { fg = mocha.surface0, bg = mocha.blue } },
+  visual = { a = { fg = mocha.surface0, bg = mocha.peach } },
+  replace = { a = { fg = mocha.surface0, bg = mocha.red } },
+
+  inactive = {
+    a = { fg = mocha.text, bg = mocha.surface0 },
+    b = { fg = mocha.text, bg = mocha.surface0 },
+    c = { fg = mocha.surface0, bg = mocha.surface0 },
+  },
+}
+
+require('lualine').setup {
+  options = {
+    theme = bubbles_theme,
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 },
+    },
+    lualine_b = {
+      { 'filename', color = { fg = mocha.teal } },
+      { 'branch', color = { fg = mocha.maroon } },
+      {
+        'diff',
+        symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
+        diff_color = {
+          added = { fg = mocha.green },
+          modified = { fg = mocha.peach },
+          removed = { fg = mocha.red },
+        },
+      },
+    },
+    lualine_c = { { 'fileformat', color = { fg = mocha.yellow } } },
+    lualine_x = {},
+    lualine_y = {
+      'filetype',
+      {
+        'o:encoding',
+        fmt = string.upper,
+        color = { fg = mocha.green, gui = 'bold' },
+      },
+      'filesize',
+      'progress',
+      'diagnostics',
+    },
+    lualine_z = {
+      { 'location', separator = { right = '' }, left_padding = 2 },
+    },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
+}
